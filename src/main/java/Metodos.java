0282dad007ebe50c595b6d1d3667ab0d42c2c7bb
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-04-01 11:00:02 +0200

    Commit inicial

---
+++ src/main/java/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+
+    // Method definition
+    public static int addNumbers(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int result = addNumbers(5, 3);
+        System.out.println("Sum: " + result); // Output: Sum: 8
+
+    }
+}
